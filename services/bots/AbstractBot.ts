b3b95991642244a7de963c3de06f254d3019d380
Beastlorion <beastlorion@gmail.com>
2023-11-29 11:17:50 -0600

    use promises instead of waiting for each set of records to come back

--- services/bots/AbstractBot.ts
+++ services/bots/AbstractBot.ts
@@ -1496,22 +1496,24 @@ abstract class AbstractBot {
     console.log("START DATE:",startDate);
     console.log("END DATE:",endDate);
     try {
-      let recordsPerRequest = 100;
-      let keepRunning = true;
+      let recordsPerRequest = 20;
+      let totalRecords = 200;
       let i = 1;
+      let promises:any = [];
       let rows: any = [];
-      while(keepRunning = true){
-        const orders: any = (await axios.get(signedApiUrl + "orders?pair=" + this.tradePairIdentifier + "&category=1" + "&periodfrom=" + startDate + "&periodto="+endDate + "&itemsperpage="+recordsPerRequest+"&pageno="+i, this.axiosConfig))
-          .data;
-        if (orders.rows.length > 0){
-          console.log("page:",i," out of:",Math.ceil(orders.rows[0].nbrof_rows/orders.rows.length));
-          rows = rows.concat(orders.rows);
-          i ++;
-        } else {
-          keepRunning = false;
-          return rows;
-        }
+      let record = await axios.get(signedApiUrl + "orders?pair=" + this.tradePairIdentifier + "&category=1" + "&periodfrom=" + startDate + "&periodto="+endDate + "&itemsperpage="+recordsPerRequest+"&pageno="+i, this.axiosConfig);
+      totalRecords = record.data.rows[0].nbrof_rows;
+      console.log("total Records:",totalRecords);
+      while(i <= Math.ceil(totalRecords/recordsPerRequest)){
+        await new Promise(resolve => setTimeout(resolve, 100));
+        promises.push(axios.get(signedApiUrl + "orders?pair=" + this.tradePairIdentifier + "&category=1" + "&periodfrom=" + startDate + "&periodto="+endDate + "&itemsperpage="+recordsPerRequest+"&pageno="+i, this.axiosConfig));
+        i++
       }
+      const records = await Promise.all(promises);
+      records.forEach((e)=>{
+        rows = rows.concat(e.data.rows);
+      })
+      return rows;
     } catch (error: any) {
       this.logger.error(`${this.instanceName} ${error}`);
     }
