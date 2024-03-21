9ab60ca6fe847a179714f1c2881848adf061b62d
Ubuntu <ubuntu@ip-172-31-18-156.ap-southeast-1.compute.internal>
2024-03-21 10:46:28 +0000

    fix cancelOrderList inputs

--- services/bots/botLauncher.ts
+++ services/bots/botLauncher.ts
@@ -1,40 +1,72 @@
+// Use this code snippet in your app.
+// If you need more information about configurations or implementing the sample code, visit the AWS docs:
+// https://docs.aws.amazon.com/sdk-for-javascript/v3/developer-guide/getting-started.html
+
+
 import { getConfig } from "../../config";
-import encrypter from "../EncryptionService";
 const BotFactory = require("./BotFactory.js");
+const config: any = getConfig(getConfig("pair"));
 
 function cleanUpAndExit(botLaunched: any) {
   botLaunched?.cleanUpAndExit();
 }
 
-encrypter.setKey(getConfig("ENCYRPT_SECRET"), getConfig("ENCRYPT_SALT"));
+import {
+  SecretsManagerClient,
+  GetSecretValueCommand,
+} from "@aws-sdk/client-secrets-manager";
 
-const privkey = encrypter.isKeyEncrypted(getConfig("private_key"))
-  ? encrypter.dencrypt(getConfig("private_key"))
-  : getConfig("private_key");
-const bot = BotFactory.createBot(getConfig("bot_type"), { botId: getConfig("bot_id"), pairStr: getConfig("pair"), privateKey: privkey });
-bot.initialize().then(() => {
-  bot.start();
-});
-// exit handler to remove all open buy and sell orders from all simulators
-process.on("SIGINT", async () => {
-  // Needed for Local Windows Ctrl-C
-  cleanUpAndExit(bot);
+const secret_name = config.secret_name;
+
+const client = new SecretsManagerClient({
+  region: "ap-southeast-1",
 });
 
-process.on("exit", async () => {
-  cleanUpAndExit(bot);
-});
+client.send(
+    new GetSecretValueCommand({
+      SecretId: secret_name,
+      VersionStage: "AWSCURRENT", // VersionStage defaults to AWSCURRENT if unspecified
+    })
+  ).then((response)=>{
 
-process.on("SIGTERM", async () => {
-  //Needed for AWS
-  cleanUpAndExit(bot);
-});
+  const secret: any = response.SecretString;
 
-process.on("uncaughtException", (error) => {
-  console.log(`BotManager uncaughtException happened:`, error);
-});
+  // Your code goes here
 
-process.on("unhandledRejection", (error, promise) => {
-  console.log(`BotManager We forgot to handle a promise rejection here:`, promise);
-  console.log(`BotManager The error was:`, error);
-});
+  const privkey = JSON.parse(secret)["dexalot_mm_"+getConfig("pair")];
+  const bot = BotFactory.createBot(getConfig("bot_type"), { botId: getConfig("bot_id"), pairStr: getConfig("pair"), privateKey: privkey });
+  bot.initialize().then(() => {
+    bot.start();
+  });
+  // exit handler to remove all open buy and sell orders from all simulators
+  process.on("SIGINT", async () => {
+    // Needed for Local Windows Ctrl-C
+    cleanUpAndExit(bot);
+  });
+
+  process.on("exit", async () => {
+    cleanUpAndExit(bot);
+  });
+
+  process.on("SIGTERM", async () => {
+    //Needed for AWS
+    cleanUpAndExit(bot);
+  });
+
+  process.on("uncaughtException", (error) => {
+    console.log(`BotManager uncaughtException happened:`, error);
+    bot.cancelOrderList([], 100);
+  });
+
+  process.on("unhandledRejection", (error, promise) => {
+    console.log(`BotManager We forgot to handle a promise rejection here:`, promise);
+    console.log(`BotManager The error was:`, error);
+    bot.cancelOrderList([], 100);
+  });
+
+
+  }).catch((error) => {
+  // For a list of exceptions thrown, see
+  // https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
+  throw error;
+})
